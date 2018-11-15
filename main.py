e34436162c6e3203c3e8a6af56d92a787940ade5
thomasdunlap <tom.a.dunlap@gmail.com>
2018-11-14 23:44:25 -0500

    fix: changed imput_image assignment to correct name.

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def load_vgg(sess, vgg_path):
 
     tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
     graph = tf.get_default_graph()
-    image_input = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
     keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
     layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
     layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
