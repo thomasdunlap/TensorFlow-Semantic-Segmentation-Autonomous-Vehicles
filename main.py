85e954b2b9dff035630e98ef16696f17eadd97cb
thomasdunlap <tom.a.dunlap@gmail.com>
2018-11-14 23:27:21 -0500

    fix: added missing comma to conv3 in line 63 in main.py.

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     init = tf.truncated_normal_initializater(stddev = 0.01)
     reg = tf.contrib.layers.l2_regularizer(.001)
 
-    conv3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same'
+    conv3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same',
                             kernel_initializer=init, kernel_regularizer=reg)
 
     conv4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same',
